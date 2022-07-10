2deee9656244c23e9ba3a68f4eeb4adae09eb448
Jingun Jung <jingun.jung@motojin.com>
2022-07-10 15:14:04 +0900

    📜 Release 0.2.0

--- src/version.py
+++ src/version.py
@@ -1,4 +1,4 @@
 from typing import Tuple
 
-__version_info__: Tuple[int, int, int] = (0, 1, 0)
+__version_info__: Tuple[int, int, int] = (0, 2, 0)
 __version__: str = ".".join(map(str, __version_info__))
